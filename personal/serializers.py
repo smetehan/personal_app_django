6df1b317822407ea1bb2eab2c93d5197dac95099
smetehan <s.metehan.parlak@gmail.com>
2023-01-15 20:01:09 +0300

    first commit

---
+++ personal/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from .models import Department, Personnel
+from django.utils.timezone import now
+class DepartmentSerializer(serializers.ModelSerializer):
+
+
+    personal_count= serializers.SerializerMethodField()
+
+    class Meta:
+        model= Department
+        fields = ('id', 'name', 'personal_count')
+
+    def get_personal_count(self,obj):
+        return obj.personals.count()
+
+
+class PersonnelSerializer(serializers.ModelSerializer):
+    
+    days_since_joined = serializers.SerializerMethodField()
+    create_user_id = serializers.IntegerField(required = False)
+    create_user = serializers.StringRelatedField()
+    
+    class Meta:
+        model = Personnel
+        fields = "__all__"
+        
+        
+    # def create(self, validated_data):
+    #     validated_data["create_user_id"] = self.context['request'].user.id
+    #     instance = Personnel.objects.create(**validated_data)
+    #     return instance
+        
+    def get_days_since_joined(self, obj):
+        return (now() - obj.start_date).days
+
+
+
+class DepartmentPersonnelSerializer(serializers.ModelSerializer):
+    personal_count=serializers.SerializerMethodField
+    personals=PersonnelSerializer(many=True, read_only=True)
+    class Meta:
+        model=Department
+        fields=('id', 'name', 'personal_count')
+
+    def get_personnel_count(self,obj):
+        return obj.personal.count()
